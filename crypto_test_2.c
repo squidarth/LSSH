88e2dcd700d789bd29c88e70afa94a234b832b93
Sidharth Shanker <sshanker220@gmail.com>
2018-06-21 11:46:11 -0400

    Added symmetric encryption/decryption steps.

--- crypto_test_2.c
+++ crypto_test_2.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <openssl/conf.h>
 #include <openssl/opensslv.h>
 #include <openssl/evp.h>
 #include <openssl/x509.h>
@@ -8,7 +12,89 @@
 void hex (unsigned char *p, size_t n){ while(n--) printf("%02x", *p++); }
 
 void err (const char * msg){ fprintf(stderr, "%s:\n", msg); ERR_print_errors_fp(stderr); exit(1); }
+void handleErrors(void)
+{
+  ERR_print_errors_fp(stderr);
+  abort();
+}
 
+int encrypt_data(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+  unsigned char *iv, unsigned char *ciphertext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int ciphertext_len;
+
+  /* Create and initialise the context */
+  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+  /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+   * and IV size appropriate for your cipher
+   * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+   * IV size for *most* modes is the same as the block size. For AES this
+   * is 128 bits */
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+    handleErrors();
+
+  /* Provide the message to be encrypted, and obtain the encrypted output.
+   * EVP_EncryptUpdate can be called multiple times if necessary
+   */
+  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    handleErrors();
+  ciphertext_len = len;
+
+  /* Finalise the encryption. Further ciphertext bytes may be written at
+   * this stage.
+   */
+  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+  ciphertext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+  return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+  unsigned char *iv, unsigned char *plaintext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int plaintext_len;
+
+  /* Create and initialise the context */
+  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+  /* Initialise the decryption operation. IMPORTANT - ensure you use a key
+   * and IV size appropriate for your cipher
+   * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+   * IV size for *most* modes is the same as the block size. For AES this
+   * is 128 bits */
+  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+    handleErrors();
+
+  /* Provide the message to be decrypted, and obtain the plaintext output.
+   * EVP_DecryptUpdate can be called multiple times if necessary
+   */
+  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+    handleErrors();
+  plaintext_len = len;
+
+  /* Finalise the decryption. Further plaintext bytes may be written at
+   * this stage.
+   */
+  if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
+  plaintext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+  return plaintext_len;
+}
 EVP_PKEY * gen (void) {
   EVP_PKEY_CTX *pctx, *kctx;
   EVP_PKEY *params = NULL, *pkey = NULL;
@@ -69,5 +155,55 @@ int main (void){
 
   printf ("prv2/pub1="); hex(secret_key_2, len2); putchar('\n');
   /* don't bother freeing for Q&D test code */
+
+  //unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
+  /* A 128 bit IV */
+  unsigned char iv[16];
+  int randomData = open("/dev/urandom", O_RDONLY);
+  if (randomData < 0) {
+    handleErrors();
+  }
+
+  int result = read(randomData, iv, 16); 
+  if (result < 0) {
+    handleErrors();
+  }
+
+  /* Message to be encrypted */
+  unsigned char *plaintext =
+                (unsigned char *)"The quick brown fox jumps over the lazy dog";
+
+  /* Buffer for ciphertext. Ensure the buffer is long enough for the
+   * ciphertext which may be longer than the plaintext, dependant on the
+   * algorithm and mode
+   */
+  unsigned char ciphertext[128];
+
+  /* Buffer for the decrypted text */
+  unsigned char decryptedtext[128];
+
+  int decryptedtext_len, ciphertext_len;
+
+  /* Encrypt the plaintext */
+  ciphertext_len = encrypt_data (plaintext, strlen ((char *)plaintext), secret_key_1, iv,
+                            ciphertext);
+
+  /* Do something useful with the ciphertext here */
+  printf("Ciphertext is:\n");
+  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+
+  /* Decrypt the ciphertext */
+  decryptedtext_len = decrypt(ciphertext, ciphertext_len, secret_key_1, iv,
+    decryptedtext);
+
+  /* Add a NULL terminator. We are expecting printable text */
+  decryptedtext[decryptedtext_len] = '\0';
+
+  /* Show the decrypted text */
+  printf("Decrypted text is:\n");
+  printf("%s\n", decryptedtext);
+
+
   return 0;
 }
